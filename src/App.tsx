5fa8936c39a4406188cba8eecb6dc90a86b8ddfb
Christine Rosales <138593141+seenjey07@users.noreply.github.com>
2024-04-05 13:33:23 +0800

    Installed Tailwind CSS, Shadcn, and DaisyUi and configured necessary files.

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,12 @@ import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header"></header>
-    </div>
+    <>
+      <div className="App">
+        <header className="App-header"></header>
+        <h1>Pinoy Palate</h1>
+      </div>
+    </>
   );
 }
 
